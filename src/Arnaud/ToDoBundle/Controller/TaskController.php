adc465e83d135aaaea3af2f96c2ff8eae94c88b8
arnaudbey <arnaud.bey@gmail.com>
2014-08-15 11:32:31 +0200

    redirect after create and delete action

--- src/Arnaud/ToDoBundle/Controller/TaskController.php
+++ src/Arnaud/ToDoBundle/Controller/TaskController.php
@@ -2,36 +2,40 @@
 
 namespace Arnaud\ToDoBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Arnaud\ToDoBundle\Manager\TaskManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 use Doctrine\ORM\EntityManager;
 
 /**
 * Class TaskrController
 *
 * @Route(
-* "",
-* service = "arnaud.task"
+*   "",
+*   service = "arnaud.task"
 * )
 */
 class TaskController
 {
     protected $entityManager;
     protected $taskManager;
+    protected $router;
 
     public function __construct(
         EntityManager $entityManager,
-        TaskManager $taskManager
+        TaskManager $taskManager,
+        RouterInterface $router
     )
     {
         $this->entityManager = $entityManager;
         $this->taskManager = $taskManager;
+        $this->router = $router;
     }
 
     /**
-     * @Route("/tasks/")
+     * @Route("/tasks/", name="tasks")
      * @Template()
      */
     public function indexAction()
@@ -43,13 +47,28 @@ class TaskController
 
     /**
      * @Route("/task/create")
-     * @Template()
      */
     public function createAction()
     {
-        $tasks = $this->taskManager->createTask();
+        $task = $this->taskManager->createTask();
+        $url = $this->router->generate("task.show", array("taskId" => $task->getId()));
 
-        return array('tasks' => $tasks);
+        return new RedirectResponse($url);
+    }
+
+    /**
+     * @Route("/task/{taskId}/delete")
+     * @Template()
+     */
+    public function deleteAction($taskId)
+    {
+        $task = $this->entityManager->getRepository('ArnaudToDoBundle:Task')->find($taskId);
+        $this->entityManager->remove($task);
+        $this->entityManager->flush();
+
+        $url = $this->router->generate("tasks", array());
+
+        return new RedirectResponse($url);
     }
 
     /**
