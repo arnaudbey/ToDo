ff3b6750994131e418640ef6f442908614aaa61f
arnaudbey <arnaud.bey@gmail.com>
2014-08-16 17:30:17 +0200

    sensio fixes : Commented code should not be commited Unused use statement should be avoided

--- src/Arnaud/ToDoBundle/DependencyInjection/Configuration.php
+++ src/Arnaud/ToDoBundle/DependencyInjection/Configuration.php
@@ -18,12 +18,11 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('arnaud_to_do');
+        $treeBuilder->root('arnaud_to_do');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
-
         return $treeBuilder;
     }
 }
